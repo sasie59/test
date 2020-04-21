726613039a4c6d608317b1f0e8b2053078b4d36a
sasie59 <33227762+sasie59@users.noreply.github.com>
2020-04-21 07:08:10 +0000

    Deploying to gh-pages from master b0152d2b39a3d174053478addab7034d4bdbcff1

---
+++ precache-manifest.1341ea78204bf9e49fae5335e8b608b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a9d2aa25f652bb240e934a50657c670",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "012a86dbc9c335b883ab",
+    "url": "/test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "aee81e3f875bb3eb805a",
+    "url": "/test/static/js/2.4b5b8776.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.4b5b8776.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "012a86dbc9c335b883ab",
+    "url": "/test/static/js/main.5a0f0402.chunk.js"
+  },
+  {
+    "revision": "dbee95b35848d10938ed",
+    "url": "/test/static/js/runtime-main.e4fd221c.js"
+  }
+]);
